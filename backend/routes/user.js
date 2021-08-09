af9910863c2a8d732de237700964e3589eb7504e
LenkaSoma <59455376+LenkaSoma@users.noreply.github.com>
2021-08-09 13:14:53 +0200

    final

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,8 @@
 const express           = require('express');
 const router            = express.Router();
 const userCtrl          = require('../controllers/user');
-const checkPassword     = require('../middleware/checkPassword');
 
-router.post('/signup', checkPassword, userCtrl.register);
+router.post('/signup', userCtrl.register);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
